12deeefb02d24849281863d1835905aa4d6daa3f
tjennings343 <tjennings343@gmail.com>
2021-03-26 23:01:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, callBack){
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+        let element = arr[i]
+        newArr.push(callBack(element))
+    }
+    return newArr
+}
+
+function reduce(arr, callBack, start){
+    let total = (!!start) ? start : arr[0]
+    let result = (!!start) ? 0 : 1
+
+    for(; result < arr.length; result++){
+        total = callBack(arr[result], total)
+    }
+    return total
+}
